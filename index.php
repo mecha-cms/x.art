dbfdd62c9cb42a20f0a86715e60e9c1adee79f6c
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-04 21:33:20 +0700

    Naming Convention

--- index.php
+++ index.php
@@ -1,62 +1,61 @@
-<?php
+<?php namespace x\art;
 
-namespace x\art\page {
-    function script($content) {
-        if ("" === ($content = \trim($content ?? ""))) {
-            return null;
-        }
-        if (false === \strpos($content, '</script>') && false === \strpos($content, '<script ')) {
-            return '<script>' . $content . '</script>';
-        }
+function content($content) {
+    if (!$content) {
         return $content;
     }
-    function style($content) {
-        if ("" === ($content = \trim($content ?? ""))) {
-            return null;
-        }
-        if (false === \strpos($content, '</style>') && false === \strpos($content, '<link ')) {
-            return '<style media="screen">' . $content . '</style>';
-        }
+    \extract($GLOBALS, \EXTR_SKIP);
+    if (empty($page)) {
         return $content;
+    }
+    // Append custom CSS before `</head>`
+    $content = \strtr($content, ['</head>' => $page->style . '</head>']);
+    // Append custom JS before `</body>`
+    $content = \strtr($content, ['</body>' => $page->script . '</body>']);
+    return $content;
+}
+
+function page__script($content) {
+    if ("" === ($content = \trim($content ?? ""))) {
+        return null;
+    }
+    if (false === \strpos($content, '</script>') && false === \strpos($content, '<script ')) {
+        return '<script>' . $content . '</script>';
+    }
+    return $content;
+}
+
+function page__style($content) {
+    if ("" === ($content = \trim($content ?? ""))) {
+        return null;
+    }
+    if (false === \strpos($content, '</style>') && false === \strpos($content, '<link ')) {
+        return '<style media="screen">' . $content . '</style>';
+    }
+    return $content;
+}
+
+function route__page($content, $path) {
+    \extract($GLOBALS, \EXTR_SKIP);
+    $folder = \LOT . \D . 'page' . \D . \trim($path ?? $state->route, '/');
+    if ($file = \exist([
+        $folder . '.archive',
+        $folder . '.page'
+    ], 1)) {
+        $page = new \Page($file);
+        $script = $page->script;
+        $style = $page->style;
+        \State::set([
+            'is' => ['art' => $art = $script || $style],
+            'not' => ['art' => !$art]
+        ]);
     }
 }
 
-namespace x\art {
-    function content($content) {
-        if (!$content) {
-            return $content;
-        }
-        \extract($GLOBALS, \EXTR_SKIP);
-        if (empty($page)) {
-            return $content;
-        }
-        // Append custom CSS before `</head>`
-        $content = \strtr($content, ['</head>' => $page->style . '</head>']);
-        // Append custom JS before `</body>`
-        $content = \strtr($content, ['</body>' => $page->script . '</body>']);
-        return $content;
-    }
-    function route($content, $path) {
-        \extract($GLOBALS, \EXTR_SKIP);
-        $folder = \LOT . \D . 'page' . \D . \trim($path ?? $state->route, '/');
-        if ($file = \exist([
-            $folder . '.archive',
-            $folder . '.page'
-        ], 1)) {
-            $page = new \Page($file);
-            $script = $page->script;
-            $style = $page->style;
-            \State::set([
-                'is' => ['art' => $art = $script || $style],
-                'not' => ['art' => !$art]
-            ]);
-        }
-    }
-    // Temporarily disable art page by adding query string `?art=false` in URL
-    if (!\array_key_exists('art', $_GET) || !empty($_GET['art'])) {
-        \Hook::set('content', __NAMESPACE__ . "\\content", 1);
-        \Hook::set('page.script', __NAMESPACE__ . "\\page\\script", 2);
-        \Hook::set('page.style', __NAMESPACE__ . "\\page\\style", 2);
-        \Hook::set('route.page', __NAMESPACE__ . "\\route", 0);
-    }
+// Temporarily disable art page by adding query string `?art=false` in URL
+if (!\array_key_exists('art', $_GET) || !empty($_GET['art'])) {
+    \Hook::set('content', __NAMESPACE__ . "\\content", 1);
+    \Hook::set('page.script', __NAMESPACE__ . "\\page__script", 2);
+    \Hook::set('page.style', __NAMESPACE__ . "\\page__style", 2);
+    \Hook::set('route.page', __NAMESPACE__ . "\\route__page", 0);
 }
